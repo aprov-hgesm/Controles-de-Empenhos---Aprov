2fd460ad2db5ee9aa49da93cf7fb70c14c4b83fa
Moriark <marcoa.r.aquino@gmail.com>
2026-09-24 17:01:12 -0300

    feat(adm): add logistics alerts route

---
+++ app/adm-deposito/alertas/page.tsx
@@ -0,0 +1,5 @@
+import { WarehouseProtectedSurface } from '../../../features/warehouse/components/WarehouseProtectedSurface';
+
+export default function WarehouseAlertsPage() {
+  return <WarehouseProtectedSurface section="alerts" />;
+}
