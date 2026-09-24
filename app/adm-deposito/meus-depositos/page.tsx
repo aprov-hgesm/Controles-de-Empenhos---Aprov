f21d2ae2397fe0c7e5c377705d5b0e1ef5db498a
Moriark <marcoa.r.aquino@gmail.com>
2026-09-24 19:15:38 -0300

    feat(adm-ui): add Meus Depositos route

---
+++ app/adm-deposito/meus-depositos/page.tsx
@@ -0,0 +1,5 @@
+import { WarehouseProtectedSurface } from '../../../features/warehouse/components/WarehouseProtectedSurface';
+
+export default function WarehouseDepotsPage() {
+  return <WarehouseProtectedSurface section="depots" />;
+}
